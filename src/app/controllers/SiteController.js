cdbb920a58a437cc5beb8a54278e661c17a343e0
NgoGiange <ngogiang190201@gmail.com>
2023-01-31 11:22:13 +0700

    prettier + lint staged (fisrt)

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,12 @@
 class SiteController {
-    // [Get =&gt; home]
-    index(req, res) {        
-        res.render('home');
-    }
+  // [Get =&gt; home]
+  index(req, res) {
+    res.render('home');
+  }
 
-    // [Get =&gt; search]
-    search(req, res) {        
-        res.render('search');
-    }
-
-
+  // [Get =&gt; search]
+  search(req, res) {
+    res.render('search');
+  }
 }
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
-const newsRouter = require('./news')
+const newsRouter = require('./news');
 const siteRouter = require('./site');
-function routes(app){
-    app.use('/news', newsRouter)
-    app.use('/',siteRouter );
+function routes(app) {
+  app.use('/news', newsRouter);
+  app.use('/', siteRouter);
 }
 
-module.exports = routes
+module.exports = routes;
