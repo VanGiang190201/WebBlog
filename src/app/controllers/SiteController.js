e70a4a0cf1e1b2f35c511d4ab9558e58a9fd273b
NgoGiange <ngogiang190201@gmail.com>
2023-01-31 10:09:41 +0700

    Base nodejs

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,14 @@
+class SiteController {
+    // [Get =&gt; home]
+    index(req, res) {        
+        res.render('home');
+    }
+
+    // [Get =&gt; search]
+    search(req, res) {        
+        res.render('search');
+    }
+
+
+}
+module.exports = new SiteController;
